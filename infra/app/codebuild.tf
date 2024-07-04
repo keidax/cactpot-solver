2f171d27269cf833f77649d1a4aaa9f88df64b58
Gabriel Holodak <gthepiper@gmail.com>
2024-07-04 14:53:12 -0400

    Add codebuild infra

---
+++ infra/app/codebuild.tf
@@ -0,0 +1,79 @@
+resource "aws_codebuild_project" "codebuild" {
+  name          = "${local.app_name}-build-deploy"
+  service_role  = aws_iam_role.codebuild_role.arn
+  build_timeout = 10
+
+  artifacts {
+    type = "NO_ARTIFACTS"
+  }
+
+  cache {
+    type = "NO_CACHE"
+  }
+
+  source {
+    type      = "GITHUB"
+    location  = "https://github.com/keidax/cactpot-solver.git"
+    buildspec = "infra/app/buildspec.yml"
+  }
+
+  environment {
+    type                        = "LINUX_CONTAINER"
+    compute_type                = "BUILD_GENERAL1_SMALL"
+    image                       = "aws/codebuild/amazonlinux2-x86_64-standard:5.0"
+    image_pull_credentials_type = "CODEBUILD"
+
+    environment_variable {
+      name  = "BUCKET_NAME"
+      value = aws_s3_bucket.site_content.id
+    }
+  }
+}
+
+data "aws_iam_policy_document" "codebuild_assume_role" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["codebuild.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+resource "aws_iam_role" "codebuild_role" {
+  name               = "${local.app_name}-codebuild-role"
+  assume_role_policy = data.aws_iam_policy_document.codebuild_assume_role.json
+}
+
+data "aws_iam_policy_document" "codebuild_policy" {
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "logs:CreateLogGroup",
+      "logs:CreateLogStream",
+      "logs:PutLogEvents",
+    ]
+
+    resources = ["*"]
+  }
+  statement {
+    effect = "Allow"
+    actions = [
+      "s3:GetObject",
+      "s3:PutObject",
+    ]
+    resources = [
+      aws_s3_bucket.site_content.arn,
+      "${aws_s3_bucket.site_content.arn}/*",
+    ]
+  }
+}
+
+resource "aws_iam_role_policy" "codebuild_policy" {
+  role   = aws_iam_role.codebuild_role.name
+  policy = data.aws_iam_policy_document.codebuild_policy.json
+}
